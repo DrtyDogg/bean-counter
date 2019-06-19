15a6894503029aef9f0bf791c38dee0171553c18
Donnie <donnie@dmellis.com>
2019-06-19 19:17:45 -0400

    removed translate

--- boot.sh
+++ boot.sh
@@ -5,7 +5,6 @@ source venv/bin/activate
 
 #update the DB and compile the application
 flask db upgrade
-flask translate compile
 
-#Run the app
+# Run the app without a context route
 exec gunicorn -b :5000 --access-logfile - --error-logfile - budget:app
