9a1ea11215e1a1815b04ea5019dd37403e3094aa
Donnie <donnie@dmellis.com>
2019-07-31 22:46:49 -0400

    setup

--- app/__init__.py
+++ app/__init__.py
@@ -46,9 +46,11 @@ def create_app(config_class=Config):
     from app.main import bp as main_bp
     from app.errors import bp as errors_bp
     from app.auth import bp as auth_bp
+    from app.api import bp as api_bp
     app.register_blueprint(main_bp)
     app.register_blueprint(errors_bp)
     app.register_blueprint(auth_bp)
+    app.register_blueprint(api_bp, url_prefix='/api')
 
     # Import other files
     with app.app_context():
